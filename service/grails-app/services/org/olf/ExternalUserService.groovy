3c32cf06a00f7e0d717c78f64b4d99759acbbc79
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-02-11 16:25:44 +0000

    removed hardcoded widgetInstance configuration

--- service/grails-app/services/org/olf/ExternalUserService.groovy
+++ service/grails-app/services/org/olf/ExternalUserService.groovy
@@ -25,21 +25,6 @@ class ExternalUserService {
       owner: resolvedUser
     ).save(flush:true, failOnError: true);
 
-    //TODO remove the below, it just adds an example widget if none are there for testing purposes
-    def jsonSlurper = new JsonSlurper()
-    def file = new File('./src/main/okapi/tenant/sample_data/widgetInstances/example_erm_simple_search_widget.json')
-    def wi = jsonSlurper.parse(file)
-
-    WidgetDefinition definition = WidgetDefinition.findByNameAndDefinitionVersion("ERM Agreements", "v1.0.0")
-    if (dash.widgets?.size() == 0) {
-      def widgetInstance = new WidgetInstance(
-        name: "ERM example widget",
-        owner: dash,
-        definition: definition,
-        configuration: JsonOutput.toJson(wi)
-      ).save(flush: true, failOnError: true)
-    }
-
     //Refetch user in case dashboard has been added
     resolvedUser = ExternalUser.read(resolvedUser.id)
     
