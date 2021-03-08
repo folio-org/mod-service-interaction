f0b429c192653ac50c26599de8028a81cb3b844e
Ethan Freestone <ethan.freestone@k-int.co.uk>
2021-03-08 13:44:58 +0000

    added a weight to the widgetInstance

--- service/grails-app/domain/org/olf/WidgetInstance.groovy
+++ service/grails-app/domain/org/olf/WidgetInstance.groovy
@@ -11,6 +11,7 @@ class WidgetInstance implements MultiTenant<WidgetInstance> {
 
   String id
   String name
+  Integer weight
   WidgetDefinition definition
 
   String configuration
@@ -21,9 +22,28 @@ class WidgetInstance implements MultiTenant<WidgetInstance> {
                  id column: 'wins_id', generator: 'uuid2', length:36
             version column: 'wins_version'
                name column: 'wins_name'
+             weight column: 'wins_weight'
          definition column: 'wins_definition_fk'
       configuration column: 'wins_configuration'
               owner column: 'wins_owner_fk'
   }
 
+  static constraints = {
+    weight (nullable: true)
+  }
+
+  def beforeValidate() {
+    if (!this.weight) {
+      // If weight is undefined, set it to the highest weight on the owner + 1
+      def maxWeight = WidgetInstance.executeQuery(
+        """SELECT MAX(wi.weight) FROM WidgetInstance wi WHERE wi.owner.id = : ownerId"""
+      , [ownerId: this.owner.id])[0]
+
+      if (maxWeight != null) {
+        this.weight = maxWeight + 1;
+      } else {
+        this.weight = 0;
+      }
+    }
+  }
 }
