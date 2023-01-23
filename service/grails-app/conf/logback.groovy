82ca1f9736099bff33b30e1558c1ad6cd54412aa
Ian Ibbotson <ianibbo@gmail.com>
2023-01-23 09:17:23 +0000

    chore: add logback config for mod.service to enable output from bootstrap as module starts

--- service/grails-app/conf/logback.groovy
+++ service/grails-app/conf/logback.groovy
@@ -38,6 +38,7 @@ logger ('com.k_int.okapi.OkapiSchemaHandler', WARN)
 logger ('com.k_int.web.toolkit', TRACE)
 logger ('org.grails.gorm.graphql', WARN)
 logger ('mod.directory', INFO)
+logger ('mod.service', INFO)
 logger ('com.k_int.okapi', WARN)
 
 logger ('com.k_int.okapi.OkapiClient', DEBUG)
