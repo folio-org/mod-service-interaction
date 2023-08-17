c7ac405aa4cea355ba0963af63a761ab5d9a7139
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-08-17 17:08:38 +0100

    chore: Removed old displayData linked to dashboard object itself

--- service/grails-app/domain/org/olf/Dashboard.groovy
+++ service/grails-app/domain/org/olf/Dashboard.groovy
@@ -8,16 +8,9 @@ import com.k_int.web.toolkit.refdata.RefdataValue
 import org.olf.ExternalUser
 
 class Dashboard implements MultiTenant<Dashboard> {
-
   String id
   String name
   String description
-
-  /* Due to the nature of dashboard, we need some display only data
-   * To avoid having to model this and locking ourselves to a particular
-   * frontend structure, store as RAW json we can send/parse in the frontend
-   */
-  String displayData
 
   static hasMany = [ widgets: WidgetInstance ]
 
@@ -26,13 +19,11 @@ class Dashboard implements MultiTenant<Dashboard> {
         version column: 'dshb_version'
            name column:'dshb_name'
     description column: 'dshb_description'
-    displayData column: 'dshb_display_data'
         widgets cascade: 'all-delete-orphan'
   }
 
   static constraints = {
            name (nullable:true, blank:false)
     description (nullable:true, blank:false)
-    displayData (nullable: true, blank: false)
   }
 }
--- service/grails-app/migrations/service-interaction-3-0.groovy
+++ service/grails-app/migrations/service-interaction-3-0.groovy
@@ -1,9 +1,4 @@
 databaseChangeLog = {
-  changeSet(author: "efreestone (manual)", id: "2023-08-16-0940-001") {
-    addColumn (tableName: "dashboard" ) {
-      column(name: "dshb_display_data", type: "text")
-    }
-  }
   changeSet(author: "efreestone (manual)", id: "2023-08-17-1207-001") {
     createTable (tableName: "dashboard_display_data" ) {
       column(name: "ddd_id", type: "VARCHAR(36)") {
